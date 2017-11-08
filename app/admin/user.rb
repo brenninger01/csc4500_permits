a5a40dcd762f64c5845414f2b0273797aa0d37e8
brenninger01 <bballer11241@gmail.com>
2017-11-07 19:10:51 -0600

    modified some fields for admin/user

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,4 +1,32 @@
 ActiveAdmin.register User do
-	permit_params :roles
+	permit_params :roles, :password, :password_confirmation
 	filter :email
+
+	index do
+    selectable_column
+    id_column
+    column :email
+    column :last_sign_in_at
+    column :sign_in_count
+    column :roles
+    actions
+  end
+
+	 form do |f|
+    	f.inputs do
+      	f.input :email
+      	f.input :password
+      	f.input :password_confirmation
+      	f.input :roles
+    	end
+    	f.actions
+  	end
+
+  	controller do #allows the password fields to be blank and update the user email 
+  		def update_resource(object, attributes)
+    		update_method = attributes.first[:password].present? ? :update_attributes : :update_without_password
+    		object.send(update_method, *attributes)
+  		end
+	end
+
 end
