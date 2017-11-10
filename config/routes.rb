04f0c96937a7d6a06edeb370354a8215cc399ef9
tech <tech@techs-MacBook-Air.local>
2017-11-10 11:35:55 -0600

    Fixed Routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   resources :emergency_contacts
   resources :faculties
   resources :students
+  authenticated :user do
+    root 'logged_in_page#home', as: :authenticated_root
+  end
   root to: 'welcome#home'
   get 'welcome/about'
 
