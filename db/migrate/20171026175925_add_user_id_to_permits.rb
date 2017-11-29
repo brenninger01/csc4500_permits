07282251c7f1e2ae7466a42c5171d1ed42f6372e
Carl <carlgoshert@gmail.com>
2017-11-29 10:25:44 -0600

    Changed 'permit' to 'vehicle_permit' in all migrations and seed file.

--- db/migrate/20171026175925_add_user_id_to_permits.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToPermits < ActiveRecord::Migration[5.1]
-  def change
-  	add_foreign_key :permits, :users
-  end
-end
--- db/migrate/20171028005847_add_permits_to_vehicles.rb
+++
@@ -1,5 +0,0 @@
-class AddPermitsToVehicles < ActiveRecord::Migration[5.1]
-  def change
-    add_foreign_key :vehicles, :permits, column: :permit_id, primary_key: :permit_id
-  end
-end
