d2d221d9bf1a0595b3f1abb8fa292983a45f343b
Carl <carlgoshert@gmail.com>
2017-11-29 19:25:08 -0600

    Removed conflicts. Migrations now run.

--- db/migrate/20171128084204_change_permits_to_vehicle_permits.rb
+++
@@ -1,5 +0,0 @@
-class ChangePermitsToVehiclePermits < ActiveRecord::Migration[5.1]
-  def change
-  	rename_table :permits, :vehicle_permits
-  end
-end
--- db/migrate/20171128092402_fix_permit_id_to_vehicle_permit_id.rb
+++
@@ -1,5 +0,0 @@
-class FixPermitIdToVehiclePermitId < ActiveRecord::Migration[5.1]
-  def change
-  	rename_column :vehicles, :permit_id, :vehicle_permit_id
-  end
-end
